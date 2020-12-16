70771d613d9c871afb99bccb4d7b5bd61901bfb8
tunagiant <68101999+tunagiant@users.noreply.github.com>
2020-12-16 23:19:00 +0900

    1

--- app/src/main/java/com/example/toast_ex01/MainActivity.java
+++ app/src/main/java/com/example/toast_ex01/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.toast_ex01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button01;
+        Button button02;
+        Button button03;
+
+        EditText editText01;
+
+        button01 = findViewById(R.id.button01);
+        button02 = findViewById(R.id.button02);
+        button03 = findViewById(R.id.button03);
+
+        editText01 = findViewById(R.id.editText01);
+
+
+
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "HI", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "BYE", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Editable string = editText01.getText();
+                Toast.makeText(getApplicationContext(), string, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
 }
