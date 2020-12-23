8728762a3e01be2195a319c219e460b63382f9c3
tunagiant <68101999+tunagiant@users.noreply.github.com>
2020-12-23 19:17:25 +0900

    1

--- Radio_ex01/app/src/main/java/com/example/radio_ex01/MainActivity.java
+++ Radio_ex01/app/src/main/java/com/example/radio_ex01/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.radio_ex01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    RadioButton radioButton01;
+    RadioButton radioButton02;
+    RadioButton radioButton03;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioButton01 = findViewById(R.id.radioButton01);
+        radioButton01.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == true) {
+                    Toast.makeText(getApplicationContext(), "01", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
